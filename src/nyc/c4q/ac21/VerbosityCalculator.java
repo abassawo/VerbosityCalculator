70d2168311f2c3e140cd257c9ed260d1c2dc0b51
abassawo <abass.bayo@gmail.com>
2015-03-20 20:06:04 -0400

    made decimal places

--- src/nyc/c4q/ac21/VerbosityCalculator.java
+++ src/nyc/c4q/ac21/VerbosityCalculator.java
@@ -3,12 +3,14 @@ package nyc.c4q.ac21;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 /**
  * Calculate the verbosity of a text
  */
 public class VerbosityCalculator {
     public static void main(String[] args){
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
         // An abstract representation of a File
         File mobyDick = new File("/Users/c4q-Abass/Desktop/accesscode/VerbosityCalculator/Resources/MobyDick.txt");
         File taleTwo =  new File("/Users/c4q-Abass/Desktop/accesscode/VerbosityCalculator/Resources/TaleOfTwoCities.txt");
@@ -34,14 +36,14 @@ public class VerbosityCalculator {
                     sentCountDick++;
                  }
             }
-            int MobyVerb = (wordCountMoby/sentCountMoby);
-            int DickensVerb = (wordCountDick/sentCountDick);
+            double MobyVerb = (wordCountMoby/sentCountMoby);
+            double DickensVerb = (wordCountDick/sentCountDick);
             System.out.println("The total word count for a Tale of Two Cities is " + wordCountDick);
             System.out.println("The sentence count for Tale of Two Cities is " + sentCountDick + "\n");
             System.out.println("The total word count for Moby Dick is " + wordCountMoby);
             System.out.println("The sentence count for Moby Dick is "+ sentCountMoby + "\n");
-            System.out.println("Dicken's verbosity measure is (word count/ sentence count), which equals" + DickensVerb);
-            System.out.println("Melville's verbosity measure is (word/sentence count),which equals " + MobyVerb);
+            System.out.println("Dicken's verbosity measure is (word count/ sentence count), which equals " + decimalFormat.format(DickensVerb));
+            System.out.println("Melville's verbosity measure is (word/sentence count),which equals " + decimalFormat.format(MobyVerb));
         }
         catch (FileNotFoundException e){
             e.printStackTrace();
